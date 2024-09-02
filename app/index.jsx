51d6f747c9e184d9aca14fb41ed273a5008194e4
rashidulas <rashidul321alam@gmail.com>
2024-09-02 13:42:19 -0500

    CustomButton

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Image } from 'react-native';
 import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { images } from '../constants';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Visura!</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{ color: 'blue'}}>Go To Home!</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}>
+          <View className="w-full flex justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[298px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-2xl text-white font-bold text-center">
+              Explore. Record. Connect.{"\n"}
+              <Text className="text-yellow-400">Visura</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-1"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Discover a World of Stories: {"\n"}
+            Share Your Adventures with Visura.
+          </Text>
+            
+
+          </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
